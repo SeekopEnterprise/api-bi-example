f2a579f1d67f0a652462fd83e8dc615b429f3f98
German Gress <ggress@seekop.com>
2025-02-25 16:18:46 -0600

    Uso de variables de entorno, fbyatiende

--- src/funnel.py
+++ src/funnel.py
@@ -1,14 +1,19 @@
 import requests
 import time
+import os
 
-EMAIL_USER = '<YOUR_EMAIL_USER>'
-PWD_USER   = '<YOUR_PWD_USER>'
-CLIENT_ID  = '<YOUR_CLIENT_ID>'
-SECRET_KEY = '<YOUR_SECRET_KEY>'
-MARCA      = '<YOUR_MARK>'
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EMAIL_USER = os.getenv("EMAIL_USER")
+PWD_USER   = os.getenv("PWD_USER")
+CLIENT_ID  = os.getenv("CLIENT_ID")
+SECRET_KEY = os.getenv("SECRET_KEY")
+MARCA      = os.getenv("MARCA")
 
 URL_AUTH_ENDPOINT = "https://api.sicopweb.com/auth/prod/token"
-URL_ENDPOINT_SERVICE = f"https://api.sicopweb.com/funnel/dev/indicadores/nacional/detalle"
+URL_ENDPOINT_SERVICE = f"https://api.sicopweb.com/funnel/v8/indicadores/nacional/detalle"
 
 class UserCredentials:
 
@@ -68,13 +73,26 @@ headers = {
   'Authorization': f'Bearer {token}'
 }
 
+params_cac = {
+    'origen':MARCA,
+    'fbyfechaini':'20241201',
+    'fbyfechafin':'20241231',
+    'fbyatiende':'CAC'
+}
+params_dealer = {
+    'origen':MARCA,
+    'fbyfechaini':'20241201',
+    'fbyfechafin':'20241231',
+    'fbyatiende':'DEALER'
+}
+params_total = {
+    'origen':MARCA,
+    'fbyfechaini':'20241201',
+    'fbyfechafin':'20241231',
+}
 # Primer peticion para obtener datos
 current_page = 1
-common_params = {
-    'origen':MARCA,
-    'fbyfechaini':'20241001',
-    'fbyfechafin':'20241031'
-}
+common_params = params_total
 params = common_params | {
     'page': current_page
 }
@@ -110,11 +128,16 @@ print(f'Total Items: {len(fulldata)}')
 # Calculamos total de prospectos acumulados en fulldata
 total_prospectos = 0
 total_prospectos_digitales = 0
+total_prospectos_inactivos = 0
+
 for row in fulldata:
     total_prospectos += int(row['prospectos'])
     total_prospectos_digitales += int(row['leads'])
+    total_prospectos_inactivos += float(row['prospectosinactivos'])
+
 print(f'Prospectos: {total_prospectos}')
 print(f'ProspectosDigitales: {total_prospectos_digitales}')
+print(f'ProspectosInactivos: {total_prospectos_inactivos}')
 
 total_time = time.time() - start_time
 
